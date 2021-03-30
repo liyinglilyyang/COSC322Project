6b76be841fcd6fe6bd88f42ffa7e41fa0687574b
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2021-03-30 16:54:08 +0800

    Action Class completed

--- src/main/java/ubc/cosc322/Action.java
+++ src/main/java/ubc/cosc322/Action.java
@@ -6,14 +6,18 @@ public class Action {
     //this representation of action 
     Coor or,de;
     int step, direction;
+    
     public Action(Coor or, Coor de){
         this.or = or;
         this.de = de;
     }
 
     public Action(Coor or, int step, int direciton){
+        int[][] actionList ={{-1, 0},{-1, -1},{-1, 1},{0, -1},{0, 1},{1, 0},{1, -1},{1, 1}};	
         this.or = or;
-        int xd,yd;
+        int xd = or.getX()+actionList[direciton][0]*step;
+        int yd = or.getY()+actionList[direciton][1]*step;
+        this.de = new Coor(xd, yd);
     }
 
     public Coor getDe(){
--- src/main/java/ubc/cosc322/NewState.java
+++ src/main/java/ubc/cosc322/NewState.java
@@ -1,6 +1,8 @@
 package ubc.cosc322;
 
 import java.util.ArrayList;
+import java.lang.Cloneable;
+
 
 import ygraph.ai.smartfox.games.Amazon.GameBoard;
 
@@ -8,6 +10,12 @@ public class NewState implements Cloneable{
 	// game state
 	Coor[][] gameBoard = new Coor[11][11];
 	// by the nature of this game, we manually created the state for the game 
+
+    protected Object clone() throws CloneNotSupportedException{
+        
+        return super.clone();
+    }
+
 	public NewState() {
 		for(int i = 1; i<11; i++)//initialize the gameborad to be all null
             for(int j = 1 ; j<11; j++){
