1b2ae2afca9994b0702cc8d1b70e6ac96c9e40f7
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2021-04-04 21:02:04 +0800

    Test Code Completed

--- src/main/java/ubc/cosc322/StateHelperTest.java
+++ src/main/java/ubc/cosc322/StateHelperTest.java
@@ -5,18 +5,25 @@ public class StateHelperTest {
         //this is intended to be used to test stateHelper
 
         NewState testBoard = new NewState();
-        
-        for(int yi = 6; yi >=5; yi--){
-            for(int xi = 1; xi <=10; xi++){
-                testBoard.gameBoard[xi][yi].setType('A');
-            }
-        }
-        for(int yi = 10; yi >=1; yi--){
-            for(int xi = 5; xi <=6; xi++){
-                testBoard.gameBoard[xi][yi].setType('A');
-            }
-        }
+        char[][] gameBoard = {
+            {'N','N','N','N','N','N','N','N','N','N','N'},
+            {'N','N','N','N','N','N','A','N','N','N','N'},
+            {'N','N','N','N','A','N','A','N','N','N','N'},
+            {'N','N','N','N','N','A','A','N','N','B','N'},
+            {'N','W','N','A','W','A','A','A','N','A','N'},
+            {'N','N','N','A','B','A','A','N','A','A','N'},
+            {'N','N','A','A','A','A','N','A','N','N','N'},
+            {'N','N','N','N','N','A','N','N','A','N','A'},
+            {'N','A','A','A','A','A','A','N','N','N','N'},
+            {'N','A','A','A','W','A','A','B','A','A','N'},
+            {'N','A','N','B','A','N','A','A','N','N','W'}
+        };
 
+        for(int yi = 10; yi >=1; yi--){
+            for(int xi = 1; xi <=10; xi++){
+                testBoard.gameBoard[yi][xi].setType(gameBoard[11-xi][yi]);
+            }
+        }
         System.out.println(testBoard);
     }
 }
